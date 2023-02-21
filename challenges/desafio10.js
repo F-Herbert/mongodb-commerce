0ba11e3db0fd3edf9c35e93ec07a5963cc452e06
felipe <felipe_herbert@hotmail.com.br>
2023-02-21 20:50:14 -0300

    Requisito 10 concluído

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,2 @@
-db.produtos.find({ "valoresNutricionais.3.percentual": { $gte: 30, $lte: 40 } }, { nome: 1, _id: 0 });
+db.produtos.find({ "valoresNutricionais.3.percentual": 
+{ $gte: 30, $lte: 40 } }, { nome: 1, _id: 0 });
