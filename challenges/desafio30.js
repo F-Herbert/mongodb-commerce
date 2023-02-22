e60e5614bc4c2f03f70a3374501816fcef26ae71
felipe <felipe_herbert@hotmail.com.br>
2023-02-21 23:47:20 -0300

    Requisito 30 concluído

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,6 +1,2 @@
-use('commerce')
-
 db.produtos.updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
-
-
-db.produtos.find({ },{ nome: 1, _id: 0, curtidas: 1 });
+db.produtos.find({ }, { nome: 1, _id: 0, curtidas: 1 });
